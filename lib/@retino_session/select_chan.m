041dea5f66d0defce0c8541c203f5e3c1b8a5590
Centos6 <Centos6>
2011-10-18 04:35:52 -0700

    M @retino_session/select_chan.m Before: Used chanType to determine rowInd (row indexing used to calculate ensemble results. e.g. F\V where F and V are concatenated over all patches.
    After: Simply uses rs.a_chan

--- lib/@retino_session/select_chan.m
+++ lib/@retino_session/select_chan.m
@@ -1,17 +1,19 @@
 function out = select_chan(obj, chanType, iRetinoPatch)
-	switch chanType
-		case 'meg'
-			a_chan = obj.meg_chan;
-		case 'eeg'
-			a_chan = obj.eeg_chan;
-		case 'meeg'
-			a_chan = [obj.meg_chan obj.eeg_chan];
-		otherwise
-			error('Must define channel type');
-	end
+% 	switch chanType
+% 		case 'meg'
+% 			a_chan = obj.meg_chan;
+% 		case 'eeg'
+% 			a_chan = obj.eeg_chan;
+% 		case 'meeg'
+% 			a_chan = [obj.meg_chan obj.eeg_chan];
+% 		otherwise
+% 			error('Must define channel type');
+% 	end
+    
 	if nargin < 3
 		iRetinoPatch = 1;
-	end
+    end
+    a_chan = obj.a_chan;
 	nChan = size(a_chan,2);
 	out = (1:nChan)+(iRetinoPatch-1)*nChan;
 end
