feaabe6fbe0689768ff1b615767bc8c4332cbcfb
dk <dk@cyclops>
2012-05-01 16:48:03 -0700

    gmp merged into retino_curveset_gui removing temp files

--- gmp.m
+++ gmp.m
@@ -1,18 +1,15 @@
 function p=gmp(o)
-pv=[
-    10*ones(10,1) linspace(100,500,10)' repmat(70, 10, 1) repmat(20, 10, 1)
-    ];
+n_button = 15;
+pv=[repmat(.01, n_button, 1) linspace(.9,0,n_button)' repmat(.1, n_button, 1) repmat(.05, n_button, 1)];
 p.fh=171;
 p.uh=uicontrol('position', [0 0 1 1],'style','text','fontname','courier new','fontsize',10,'horizontalalignment','left');
-p.bh(1)=uicontrol('position', pv(1,:), 'string', 'Add', 'callback', {@key_fcn, o, 'a'});
-p.bh(2)=uicontrol('position', pv(2,:), 'string', 'Set', 'callback', {@key_fcn, o, 's'});
-p.bh(3)=uicontrol('position', pv(3,:), 'string', 'Delete', 'callback', {@key_fcn, o, 'd'});
-p.bh(4)=uicontrol('position', pv(4,:), 'string', 'View Retino', 'callback', {@key_fcn, o, '['});
-p.bh(5)=uicontrol('position', pv(5,:), 'string', 'View ROI', 'callback', {@key_fcn, o, ']'});
-p.bh(6)=uicontrol('position', pv(6,:), 'string', 'View Nodes', 'callback', {@key_fcn, o, '\'});
-
+p.bh(1)=uicontrol('unit', 'norm', 'position', pv(1,:), 'string', 'Add', 'callback', {@key_fcn, o, 'a'});
+p.bh(2)=uicontrol('unit', 'norm', 'position', pv(2,:), 'string', 'Set', 'callback', {@key_fcn, o, 's'});
+p.bh(3)=uicontrol('unit', 'norm', 'position', pv(3,:), 'string', 'Delete', 'callback', {@key_fcn, o, 'd'});
+p.bh(4)=uicontrol('unit', 'norm', 'position', pv(4,:), 'string', 'Retino View', 'callback', {@key_fcn, o, '['});
+p.bh(5)=uicontrol('unit', 'norm', 'position', pv(5,:), 'string', 'ROI View', 'callback', {@key_fcn, o, ']'});
+p.bh(6)=uicontrol('unit', 'norm', 'position', pv(6,:), 'string', 'Nodes View', 'callback', {@key_fcn, o, '\'});
 set(p.fh, 'keypressfcn', {@key_fcn, o});
-
 return;
 %-----------------------------------------
 function key_fcn(h_fig, evnt, o, id)
@@ -52,10 +49,12 @@ switch key
             set(o.rs.h.rois.all.p, 'visible', 'on')
         end
     case {'\'}
-        if  isequal(get(o.rs.h.rois.all.p(1), 'visible'), 'on')
-            set(o.rs.h.rois.all.p, 'visible', 'off')
+        if  isequal(get(o.rs.h.nodes.lh, 'visible'), 'on')
+            set(o.rs.h.nodes.lh, 'visible', 'off')
+            set(o.rs.h.nodes.rh, 'visible', 'off')
         else
-            set(o.rs.h.rois.all.p, 'visible', 'on')
+            set(o.rs.h.nodes.lh, 'visible', 'on')
+            set(o.rs.h.nodes.rh, 'visible', 'on')
         end
     otherwise
 end
